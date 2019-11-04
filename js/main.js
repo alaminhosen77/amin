7dd10d0002bb757014ec1188fe5b0b42594e20ec
alaminhosen143 <alaminhosen143>
2019-11-04 23:51:14 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    $(".testimonial-active").owlCarousel({
+        items:1,
+        loop:true,
+        nav:true,
+        autoplay:true,
+        autoplaySpeed:2000,
+        dots:false,
+    });
+  });
